5ae2184bed9962026b55b1fa3ab1ffc1745ad33c
erik-a-bensen <bensen.erik.a@gmail.com>
2024-05-13 10:33:43 -0400

    removed parallel

--- src/tmg_hmc/sampler.py
+++ src/tmg_hmc/sampler.py
@@ -2,12 +2,8 @@ import numpy as np
 import ray
 from scipy.sparse import csc_matrix
 from typing import Protocol, Tuple
-
 from tmg_hmc.constraints import Constraint, LinearConstraint, SimpleQuadraticConstraint, QuadraticConstraint
 from tmg_hmc.utils_quartic import nanmin, nanargmin
-
-def single_hit_time(c: Constraint, x: np.ndarray, xdot: np.ndarray) -> float:
-    return c.hit_time(x, xdot)
 
 class TMGSampler:
     """
@@ -72,11 +68,7 @@ class TMGSampler:
         return xnew, xdotnew
     
     def _hit_time(self, x: np.ndarray, xdot: np.ndarray) -> Tuple[float, Constraint]:
-        if self.parallel:
-            remote_times = [self.phit_time.remote(c, x, xdot) for c in self.constraints]
-            times = ray.get(remote_times)
-        else:
-            times = np.array([c.hit_time(x, xdot) for c in self.constraints])
+        times = np.array([c.hit_time(x, xdot) for c in self.constraints])
         ind = nanargmin(times)
         if ind is None:
             return np.nan, None
@@ -84,73 +76,33 @@ class TMGSampler:
     
     def _iterate(self, x: np.ndarray, xdot: np.ndarray, verbose: bool) -> np.ndarray:
         t = 0
-        # print(f"Initial x: {x}")
-        # print(f"Initial xdot: {xdot}")
         cprev = None
         h, c = self._hit_time(x, xdot)
-        # print(f"h: {h}")
         i = 0
         while not (h > self.T - t or np.isnan(h)):
             i += 1
             x, xdot = self._propagate(x, xdot, h)
-            # print(f"constraint value: {c.value(x)}")
-            # self._constraints_satisfied(x)
-            # print(f"x: {x}")
-            # print(f"xdot: {xdot}")
-            # if c.value(x) < -0.0:
-            #     epsilon = 1e-10
-            #     x, xdot = self._propagate(x, xdot, -epsilon)
-            #     t -= epsilon
-            # print(f"constraint value: {c.value(x)}")
             if c.is_zero(x):
                 xdot = c.reflect(x, xdot)
             t += h
             cprev = c
             h, c = self._hit_time(x, xdot)
-        #     print(f"h: {h}")
-        # print(f"trem: {self.T - t}")
-        # print(f"x: {x}")
-        # print(f"xdot: {xdot}")
         x, xdot = self._propagate(x, xdot, self.T - t)
-        # print(f"final x: {x}")
         if verbose:
             print(f"\tNumber of collision checks: {i}")
         if not self._constraints_satisfied(x):
-            print(f"cprev: {cprev.value(x)}")
             raise ValueError("Error at final step")
         return x
-    
-    def _start_pool(self, n_threads: int) -> None:
-        ray.init()
-        if n_threads > ray.cluster_resources()["CPU"]:
-            raise Warning(f"Requested number of threads ({n_threads}) is greater than the number of available CPUs ({mp.cpu_count()})")
-            n_threads = ray.cluster_resources()["CPU"]
-        elif n_threads == -1:
-            n_threads = ray.cluster_resources()["CPU"]
-        elif n_threads < 1:
-            raise ValueError("Number of threads must be at least 1")
-        self.parallel = True
-        
-        @ray.remote(num_cpus=n_threads)
-        def hit_time(c: Constraint, x: np.ndarray, xdot: np.ndarray) -> float:
-            return c.hit_time(x, xdot)
-        self.phit_time = hit_time
-    
-    def _close_pool(self) -> None:
-        self.parallel = False
-        ray.shutdown()
     
     def sample_xdot(self) -> np.ndarray:
         return self.Sigma_half @ np.random.standard_normal(self.dim).reshape(self.dim, 1)
             
-    def sample(self, x0: np.ndarray, n_samples: int, burn_in: int = 100, threads: int = None, verbose=False) -> np.ndarray:
+    def sample(self, x0: np.ndarray, n_samples: int, burn_in: int = 100, verbose=False) -> np.ndarray:
         x0 = x0.reshape(self.dim, 1)
         # x0 = self.Sigma_inv_half @ (x0 - self.mu)
         if not self._constraints_satisfied(x0):
             raise ValueError("Initial point does not satisfy constraints")
         samples = np.zeros((n_samples, self.dim))
-        if threads is not None:
-            self._start_pool(threads)
         x = x0
         for i in range(burn_in):
             if verbose:
@@ -164,6 +116,4 @@ class TMGSampler:
             x = self._iterate(x, xdot, verbose)
             # samples[i,:] = (self.Sigma_half@x).flatten()
             samples[i,:] = x.flatten()
-        if threads is not None:
-            self._close_pool()
         return samples
