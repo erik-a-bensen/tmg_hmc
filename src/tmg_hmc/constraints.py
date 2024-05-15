added binary search refinement to correct compounding numerical errors

src/tmg_hmc/constraints.py
@@ -4,16 +4,17 @@ from typing import Protocol, Tuple
 from tmg_hmc.utils import soln1, soln2, soln3, soln4, soln5, soln6, soln7, soln8
 
 pis = np.array([-1, 0, 1]) * np.pi
-eps = 1e-10
+eps = 1e-12
 
 class Constraint(Protocol):
     def value(self, x: np.ndarray) -> float:...
         
     def is_satisfied(self, x: np.ndarray) -> bool:
-        return (self.value(x) > 0) or np.isclose(self.value(x), 0)
+        return self.value(x) >= 0 #or np.isclose(self.value(x), 0)
 
-    def is_zero(self, x: np.ndarray) -> bool:
-        return np.isclose(self.value(x), 0)
+    def is_zero(self, x: np.ndarray) -> Tuple[bool, bool]:
+        val = self.value(x)
+        return np.isclose(val, 0), np.isclose(val, 0, atol=1e-3)
 
     def hit_time(self, a: np.ndarray, b: np.ndarray) -> np.ndarray:...
 
@@ -99,9 +100,6 @@ class QuadraticConstraint(Constraint):
         self.A = A
         self.b = b
         self.c = c
-
-    def is_zero(self, x: np.ndarray) -> bool:
-        return np.isclose(self.value(x), 0)#, atol=1e-1)
     
     def value(self, x: np.ndarray) -> float:
         return x.T @ self.A @ x + self.b.T @ x + self.c
@@ -122,7 +120,7 @@ class QuadraticConstraint(Constraint):
 
     def hit_time(self, x: np.ndarray, xdot: np.ndarray) -> np.ndarray:
         a, b = xdot, x
-        pis = np.arange(-16,17)*np.pi/8
+        pis = np.array([-2, 0, 2])*np.pi
         qs = self.compute_q(a, b)
         print(f"qs: {qs}")
         s1 = soln1(*qs) + pis
@@ -135,4 +133,4 @@ class QuadraticConstraint(Constraint):
         s8 = soln8(*qs) + pis
         s = np.hstack([s1, s2, s3, s4, s5, s6, s7, s8])
         #print(f"s: {s}")
-        return np.unique(s[s > eps])
+        return np.unique(s[s > 1e-6])

src/tmg_hmc/sampler.py
@@ -86,13 +86,31 @@ class TMGSampler:
         inds = np.argsort(times)
         return times[inds], cs[inds]
     
+    def _binary_search(self, x: np.ndarray, xdot: np.ndarray, b1: float, b2: float, c: QuadraticConstraint) -> Tuple[np.ndarray, np.ndarray, float, bool]:
+        x1, _ = self._propagate(x, xdot, b1)
+        hmid = (b1 + b2) / 2
+        xmid, xdotmid = self._propagate(x, xdot, hmid)
+        if np.isclose(c.value(xmid), 0):
+            return xmid, xdotmid, hmid, True
+        if np.sign(c.value(xmid)) != np.sign(c.value(x1)):
+            return self._binary_search(x, xdot, b1, hmid, c)
+        return self._binary_search(x, xdot, hmid, b2, c)
+    
+    def _refine_hit_time(self, x: np.ndarray, xdot: np.ndarray, c: QuadraticConstraint) -> Tuple[np.ndarray, np.ndarray, float, bool]:
+        sign = np.sign(c.value(x))
+        h = 1e-3 * sign
+        x_temp, _ = self._propagate(x, xdot, h)
+        if np.sign(c.value(x_temp)) == sign:
+            return x, xdot, 0, False
+        return self._binary_search(x, xdot, 0, h, c)
+    
     def _iterate(self, x: np.ndarray, xdot: np.ndarray, verbose: bool) -> np.ndarray:
         t = 0 
         i = 0
         hs, cs = self._hit_times(x, xdot)
         h, c = hs[0], cs[0]
-        print(f"Initial x: {x.flatten()}")
-        print(f"Initial xdot: {xdot.flatten()}")
+        print(f"Initial x: ({x[0,0]}, {x[1,0]})")
+        print(f"Initial xdot: ({xdot[0,0]}, {xdot[1,0]})")
         while h < self.T - t:
             i += 1
             inds = hs < self.T - t
@@ -103,31 +121,29 @@ class TMGSampler:
                 h, c = hs[pos], cs[pos]
                 x_temp, xdot_temp = self._propagate(x, xdot, h)
                 print(f"cvalue: {c.value(x_temp)}")
-                if c.is_zero(x_temp):
+                zero, refine = c.is_zero(x_temp)
+                if refine and not zero:
+                    x_temp, xdot_temp, h_adj, zero = self._refine_hit_time(x_temp, xdot_temp, c)
+                    h += h_adj
+                if zero:
+                    # if not c.is_satisfied(x_temp):
+                    #     h -= 1e-3
+                    #     x_temp, xdot_temp = self._propagate(x, xdot, h)
+                    #     print(f"cvalue adjusted: {c.value(x_temp)}")
                     x, xdot = x_temp, xdot_temp
                     xdot = c.reflect(x, xdot)
-                    print(f"Propagated x: {x.flatten()}")
-                    print(f"Reflected xdot: {xdot.flatten()}")
+                    print(f"Hit time: {h}")
+                    print(f"Propagated x: ({x[0,0]}, {x[1,0]})")
+                    print(f"Reflected xdot: ({xdot[0,0]}, {xdot[1,0]})")
                     t += h
                     break
-                elif not c.is_satisfied(x_temp):
-                    if pos > 0:
-                        h = hs[pos-1]
-                        c = cs[pos-1]
-                        x, xdot = self._propagate(x, xdot, h)
-                        t += h
-                        print(f"Propagated x: {x.flatten()}")
-                        print(f"Propagated xdot: {xdot.flatten()}")
-                        break
-                    else:
-                        raise ValueError("Error finding constraint hit time")
             else:
                 break
             hs, cs = self._hit_times(x, xdot)
             h, c = hs[0], cs[0]
         x, xdot = self._propagate(x, xdot, self.T - t)
-        print(f"Final x: {x.flatten()}")
-        print(f"Final xdot: {xdot.flatten()}")
+        print(f"Final x: ({x[0,0]}, {x[1,0]})")
+        print(f"Final xdot: ({xdot[0,0]}, {xdot[1,0]})")
         if verbose:
             print(f"\tNumber of collision checks: {i}")
         if not self._constraints_satisfied(x):

src/tmg_hmc/utils.py
@@ -6,11 +6,11 @@ np.seterr(divide='ignore', invalid='ignore')
 
 def arccos(x: float) -> float:
     val = acos(x)
-    if np.isclose(val.imag, 0):
-        return val.real
-    else:
+    if not np.isclose(val.imag, 0):
         return np.nan
-    # return val.real
+    if np.isclose(val.real, 0):
+        return 0
+    return val.real
 
 def soln1(q1: float, q2: float, q3: float, q4: float, q5: float) -> float:
     return (-arccos(-0.5*(q1*q2 + q4*q5)/(q1**2 + q4**2) - 