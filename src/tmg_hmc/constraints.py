fixed cpp calculation to use complex<double> type until the end

src/tmg_hmc/constraints.py
@@ -243,6 +243,7 @@ class QuadraticConstraint(BaseQuadraticConstraint):
 
         if self.sparse:
             self._setup_values_sparse(A, S)
+            self.S = S
         else:
             self._setup_values(A, S)
         if self.compiled:
@@ -315,17 +316,18 @@ class QuadraticConstraint(BaseQuadraticConstraint):
 
     def hit_time_cpp(self, x: Array, xdot: Array) -> Array:
         a, b = xdot, x
-        pis = np.array([-2, 0, 2]).reshape(3,1)*np.pi
+        pis = np.array([-2, 0, 2]).reshape(-1,1)*np.pi
         qs = self.compute_q(a, b)
         soln = lib.calc_all_solutions(*qs)
         s = np.ctypeslib.as_array(soln, shape=(1,8))
+        # print(s)
         lib.free_ptr(soln)
         s = (s + pis).flatten()
-        return np.unique(s[s > eps])#1e-8])
+        return np.unique(s[s > 1e-7])
     
     def hit_time_py(self, x: Array, xdot: Array) -> Array:
         a, b = xdot, x
-        pis = np.array([-2, 0, 2]).reshape(3,1)*np.pi
+        pis = np.array([-2, 0, 2]).reshape(-1,1)*np.pi
         qs = self.compute_q(a, b)
         s1 = soln1(*qs) + pis
         s2 = soln2(*qs) + pis
@@ -336,7 +338,7 @@ class QuadraticConstraint(BaseQuadraticConstraint):
         s7 = soln7(*qs) + pis
         s8 = soln8(*qs) + pis
         s = np.hstack([s1, s2, s3, s4, s5, s6, s7, s8])
-        return np.unique(s[s > eps])#1e-8])
+        return np.unique(s[s > 1e-7])
     
 if __name__ == "__main__":
     test_constr = QuadraticConstraint(np.array([[1, 0], [0, 1]]), np.array([0, 0]), 0, np.array([[1, 0], [0, 1]]))

src/tmg_hmc/sampler.py
@@ -146,14 +146,16 @@ class TMGSampler:
         x1, _ = self._propagate(x, xdot, b1)
         hmid = (b1 + b2) / 2
         xmid, xdotmid = self._propagate(x, xdot, hmid)
-        if np.isclose(c.value(xmid),0.):
+        x2, _ = self._propagate(x, xdot, b2)
+        if np.isclose(c.value(xmid),0., atol=1e-12):
             return xmid, xdotmid, hmid, True
         if np.sign(c.value(xmid)) != np.sign(c.value(x1)):
             return self._binary_search(x, xdot, b1, hmid, c)
         return self._binary_search(x, xdot, hmid, b2, c)
     
     def _refine_hit_time(self, x: Array, xdot: Array, c: QuadraticConstraint) -> Tuple[Array, Array, float, bool]:
-        sign = np.sign(c.value(x))
+        value = c.value(x)
+        sign = np.sign(value)
         h = 1e-3 * sign
         x_temp, _ = self._propagate(x, xdot, h)
         if np.sign(c.value(x_temp)) == sign:
@@ -195,6 +197,7 @@ class TMGSampler:
         self.constraint_violations += 1
         if verbose:
             print(f"Constraint violated, redoing iteration")
+
         xdot = self.sample_xdot()
         return self._iterate(x_init, xdot, verbose)
     